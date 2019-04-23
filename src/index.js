2a927030b3efb839213ab467b9eb254fd4885b8a
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2019-04-22 22:40:53 -0500

    tic-tac-toe Project: X and O added
    Added the this.state to identify X and O

--- src/index.js
+++ src/index.js
@@ -2,10 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-class Square extends React.Component {
-  render() {
-    return <button className="squareButton">{this.props.value} </button>;
-  }
+function Square(props) {
+  return (
+    <button
+      className="squareButton"
+      onClick={() => {
+        props.onClick();
+      }}
+    >
+      {props.value}
+    </button>
+  );
 }
 
 // const App = () => {
@@ -13,31 +20,56 @@ class Square extends React.Component {
 // };
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+      xIsNext: true
+    };
+  }
   renderSquares(i) {
-    return <Square value={i} />;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = this.state.xIsNext ? "X" : "O";
+    this.setState({ squares: squares, xIsNext: !this.state.xIsNext });
   }
 
   render() {
+    const status = "Next Player : " + (this.state.xIsNext ? "X" : "O");
+
     return (
-      <div className="displayBoard">
-        <div className="rowBreak">
-          {this.renderSquares(1)}
-          {this.renderSquares(2)}
-          {this.renderSquares(3)}
-        </div>
-        <div className="rowBreak">
-          {this.renderSquares(4)}
-          {this.renderSquares(5)}
-          {this.renderSquares(6)}
-        </div>
-        <div className="rowBreak">
-          {this.renderSquares(7)}
-          {this.renderSquares(8)}
-          {this.renderSquares(9)}
+      <div>
+        <div className="displayBoard">
+          {status}
+          <div className="rowBreak">
+            {this.renderSquares(0)}
+            {this.renderSquares(1)}
+            {this.renderSquares(2)}
+          </div>
+          <div className="rowBreak">
+            {this.renderSquares(3)}
+            {this.renderSquares(4)}
+            {this.renderSquares(5)}
+          </div>
+          <div className="rowBreak">
+            {this.renderSquares(6)}
+            {this.renderSquares(7)}
+            {this.renderSquares(8)}
+          </div>
         </div>
       </div>
     );
   }
 }
+
+// ========================================
 
 ReactDOM.render(<Board />, document.getElementById("root"));
